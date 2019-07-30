a62659e600c8cbb3392277920a04d528a3e87715
Arturs Elksnis <arturs@elksnis.co.uk>
2019-07-30 16:38:06 +0100

    Quiz 1 Uniform Distribution

---
+++ Lesson1-Localization_Overview/quizes/Uniform_Distribution.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# Modify the empty list, p, so that it becomes a UNIFORM probability
+# distribution over five grid cells, as expressed in a list of 
+# five probabilities.
+
+p = []
+
+print p
