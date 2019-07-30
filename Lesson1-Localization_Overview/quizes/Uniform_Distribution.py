5af6e0dc3f8c67ad464684200e8de09ae10ebdef
Arturs Elksnis <arturs@elksnis.co.uk>
2019-07-30 17:20:27 +0100

    Multiplying cells 1 and 2 with pHit and the rest of the cells with pMiss

--- Lesson1-Localization_Overview/quizes/Uniform_Distribution.py
+++ Lesson1-Localization_Overview/quizes/Uniform_Distribution.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
-#  Modify your code to create probability vectors, p, of arbitrary 
-#  size, n. Use n=5 to verify that your new solution matches 
-#  the previous one.
+#Write code that outputs p after multiplying each entry 
+#by pHit or pMiss at the appropriate places. Remember that
+#the red cells 1 and 2 are hits and the other green cells
+#are misses.
 
-p=[]
-n=5
 
-for i in range(n):
-	p.append(1.0/n)
+p=[0.2,0.2,0.2,0.2,0.2]
+pHit = 0.6
+pMiss = 0.2
+
+#Enter code here
+
+for i in range(len(p)):
+	if i == 1 or i == 2:
+		p[i] = p[i] * pHit
+	else:
+		p[i] = p[i] * pMiss
 
 print p
