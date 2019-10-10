67240267ccca4d2d63f3a59c4e606d3697017bd1
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-10-10 18:50:21 +0100

    Added more comments and implemented move and sense twice

--- Lesson1-Localization_Overview/quizes/Uniform_Distribution.py
+++ Lesson1-Localization_Overview/quizes/Uniform_Distribution.py
@@ -4,11 +4,13 @@
 #measurements are incorporated. Make sure that your code 
 #allows for any sequence of measurement of any length.
 
-p=[0, 1, 0, 0, 0]
-world=['green', 'red', 'red', 'green', 'green']
-measurements = ['red', 'green']
-pHit = 0.6
-pMiss = 0.2
+p=[0, 1, 0, 0, 0] # where the robot is (probabilities for each field in the world)
+world=['green', 'red', 'red', 'green', 'green'] # the map of the world- true colours of each field
+measurements = ['red', 'green'] # what was measured both times that we measuered
+pHit = 0.6 # "caution" factor with which we are going to multiply a measurement, that we expected (adds to our belief about a state)
+pMiss = 0.2 # "caution" factor with which we are going to multiply a mesaurement, that we didn't expect (adds doubt to our belief about a state)
+
+motions = [1, 1] # In one of the scenarios the robot moves twice to the right by 1 step each time
 
 pExact = 0.8
 pOvershoot = 0.1
@@ -26,6 +28,7 @@ def sense(p, Z):
     for i in range(len(world)):
         hit = (Z == world[i])
         q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+        #print hit, " ", q, " ", p[i]
     
     # Now normalisation
     q_sum = sum(q)
@@ -53,17 +56,33 @@ def move(p, U):
         q.append(new_val)
     return q
 
-print move(p, 2)
+print "Initial world: ", p
+p = move(p, 2)
+print "After moving two steps to the right: ", p
     
 # applying the measured result to the probability vector:
 for i in range(len(measurements)):
     p = sense(p, measurements[i])
-print p
+
+print "After subsequent sense: ", p, " Because the measurements were contradictory, we get back the same probability vector"
 
 # printing what the world will look like if robot moves twice without anymore sensing
-print move(move(p, 1),1)
+print "After two more moves by 1 step to the right without sensing anymore: ", move(move(p, 1),1)
 
 # Now move 1000 times and see how certainty decays to uniform distribution of 0.2
 for i in range(1000):
     p = move(p, 1)
-print p
+
+print "After 1000 more moves to the right without sensing: ", p
+
+#
+# Now robot first senses red, then moves right one, then senses 
+# green, then moves right again, starting with a uniform prior distribution.
+for m in range(len(motions)):
+    p = sense(p, measurements[m])
+    p = move(p, motions[m])
+
+print "Robot moves to the right, then senses red, then moves right again and senses green (starts at uniform disitribution): ", p
+print "The world is always: ", world
+
+print "And now according to our algorithm the highest possibility for the robot is to be at cell 5 (right most)"
