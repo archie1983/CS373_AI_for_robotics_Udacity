516c730cb37934bd54850f702974cc6f56823b84
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-10-30 16:04:08 +0000

    Gaussian mean and variance update complete

--- Lesson1-Localization_Overview/quizes/Gaussians_Mean_Variance_KallmanFilters.py
+++ Lesson1-Localization_Overview/quizes/Gaussians_Mean_Variance_KallmanFilters.py
@@ -6,8 +6,8 @@
 # belief function.
 
 def update(mean1, var1, mean2, var2):
-    new_mean = 
-    new_var = 
+    new_mean = (mean1 * var2 + mean2 * var1) / (var1 + var2)
+    new_var = 1 / (1 / var1 + 1 / var2)
     return [new_mean, new_var]
 
 print update(10.,8.,13., 2.)
