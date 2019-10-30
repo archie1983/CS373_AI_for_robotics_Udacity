1749732fca755eecf4fbe880b47121e8a684282e
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-10-30 10:18:32 +0000

    Added Sebastian Thrun's solution (more elegant)

--- Lesson1-Localization_Overview/homeworks/HomeWork1-LocalizationProgram.py
+++ Lesson1-Localization_Overview/homeworks/HomeWork1-LocalizationProgram.py
@@ -278,3 +278,28 @@ show(p) # displays your answer
 # p_move = 0.5
 # p = localize(colors,measurements,motions,sensor_right,p_move)
 #show(p) # displays your answer
+
+#
+# Sebastian Thrun's very elegant solution:
+#
+def sense(p, colors, measurement):
+    aus = [[0.0 for row in range(len(p[0]))] for col in range(len(p))]
+    
+    s = 0.0
+    for i in range(len(p)):
+        for j in range(len(p[i])):
+            hit = (measurement == colors[i][j])
+            aux[i][j] = p[i][j] * (hit * sensor_right + (1 - hit) * sensor_wrong)
+            s += aux[i][j]
+    for i in range (len(aux)):
+        for j in range(len(p[i])):
+            aux[i][j] /= s
+    return aux
+    
+def move(p, motion):
+    aux = [[0.0 for row in range(len(p[0]))] for col in range(len(p))]
+    
+    for i in range(len(p)):
+        for j in range(len(p[i])):
+            aux[i][j] = (p_move * p[(i - motion[0]) % len(p)][(j - motion[1]) % len(p[i])]) + (p_stay * p[i][j])
+    return aux
