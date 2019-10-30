44ede71b085b80d8bc8fd20c2f00b29740095141
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-10-30 16:18:08 +0000

    Starting an sense-move exercise with given measurements and movements using Kalman filter.

--- Lesson1-Localization_Overview/quizes/Gaussians_Mean_Variance_KallmanFilters.py
+++ Lesson1-Localization_Overview/quizes/Gaussians_Mean_Variance_KallmanFilters.py
@@ -19,5 +19,22 @@ def predict(mean1, var1, mean2, var2):
     new_var = var1 + var2
     return [new_mean, new_var]
 
-print update(10.,8.,13., 2.) # measurement
-print predict(10., 4., 12., 4.) # motion
+#print update(10.,8.,13., 2.) # measurement
+#print predict(10., 4., 12., 4.) # motion
+
+# Write a program that will iteratively update and
+# predict based on the location measurements 
+# and inferred motions shown below. 
+measurements = [5., 6., 7., 9., 10.]
+motion = [1., 1., 2., 1., 1.]
+measurement_sig = 4.
+motion_sig = 2.
+mu = 0.
+sig = 10000.
+
+#Please print out ONLY the final values of the mean
+#and the variance in a list [mu, sig]. 
+
+# Insert code here
+
+print [mu, sig]
