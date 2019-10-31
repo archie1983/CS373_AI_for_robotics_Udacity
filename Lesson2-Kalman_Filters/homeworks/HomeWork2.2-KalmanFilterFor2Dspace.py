a5f66373c457f4a884696c81454ddc512162d644
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-10-31 16:36:03 +0000

    Homework 2.2 almost works, but result is slightly different from the lecturer's.

--- Lesson2-Kalman_Filters/homeworks/HomeWork2.2-KalmanFilterFor2Dspace.py
+++ Lesson2-Kalman_Filters/homeworks/HomeWork2.2-KalmanFilterFor2Dspace.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # Fill in the matrices P, F, H, R and I at the bottom
 #
 # This question requires NO CODING, just fill in the 
@@ -153,6 +155,15 @@ def filter(x, P):
         
         # measurement update
         Z = matrix([measurements[n]])
+        # print "Z:"
+        # Z.show()
+        # print "H:"
+        # H.show()
+        # print "x:"
+        # x.show()
+        # print "Hx:"
+        # Hx = H * x
+        # Hx.show()
         y = Z.transpose() - (H * x)
         S = H * P * H.transpose() + R
         K = P * H.transpose() * S.inverse()
@@ -185,11 +196,30 @@ u = matrix([[0.], [0.], [0.], [0.]]) # external motion
 #### DO NOT MODIFY ANYTHING ABOVE HERE ####
 #### fill this in, remember to use the matrix() function!: ####
 
-P =  # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
-F =  # next state function: generalize the 2d version to 4d
-H =  # measurement function: reflect the fact that we observe x and y but not the two velocities
-R =  # measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
-I =  # 4d identity matrix
+P =  matrix([[1000., 1000., 0., 0.], 
+             [0, 1000, 1000., 0.], 
+             [0, 0, 1000, 1000], 
+             [0, 1000, 1000, 0]]) # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
+P =  matrix([[1000, 0, 1000, 0], 
+             [0, 1000, 1000, 0], 
+             [1000, 0, 1000, 0], 
+             [0, 1000, 1000, 0]]) # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
+P =  matrix([[1000., 0., 0., 0.], 
+             [0., 1000., 0., 0.], 
+             [0., 0., 1000., 0.], 
+             [0., 0., 0., 1000.]]) # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
+F =  matrix([[1., 0., dt, 0.], 
+             [0., 1., 0., dt], 
+             [0., 0., 1., 0.], 
+             [0., 0., 0., 1.]])# next state function: generalize the 2d version to 4d
+H = matrix([[1., 0., 0., 0.],
+            [0., 1., 0., 0.]]) # measurement function: reflect the fact that we observe x and y but not the two velocities
+R = matrix([[0.1, 0.],
+            [0., 0.1]]) # measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
+I = matrix([[1., 0., 0., 0.], 
+            [0., 1., 0., 0.], 
+            [0., 0., 1., 0.], 
+            [0., 0., 0., 1.]]) # 4d identity matrix
 
 ###### DO NOT MODIFY ANYTHING HERE #######
 
