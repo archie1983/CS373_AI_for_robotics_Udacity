60a307730bc5342041f300a20c2465d092a3e876
Arturs Elksnis <arturs.elksnis@ggtg.net>
2019-11-04 17:23:28 +0000

    Making robot move according to the quiz.

--- Lesson3-Particle_Filters/quizzes/moving_robot.py
+++ Lesson3-Particle_Filters/quizzes/moving_robot.py
@@ -118,3 +118,15 @@ def eval(r, p):
 
 myrobot = robot()
 
+# Make a robot called myrobot that starts at
+# coordinates 30, 50 heading north (pi/2).
+# Have your robot turn clockwise by pi/2, move
+# 15 m, and sense. Then have it turn clockwise
+# by pi/2 again, move 10 m, and sense again.
+
+myrobot.set(30, 50, pi / 2)
+#print myrobot.sense()
+myrobot = myrobot.move(-pi / 2, 15)
+print myrobot.sense()
+myrobot = myrobot.move(-pi / 2, 10)
+print myrobot.sense()
